ffcebb80eb3c44454a9d0da47831aef2240b4fda
Leo Gomez <leo.gomez@iut-rodez.fr>
2026-09-15 21:11:50 +0200

    fin TP1

--- LectureEcritureJava/image.java
+++ LectureEcritureJava/image.java
@@ -1,11 +1,13 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class Image {
     private int width;
     private int height;
     // pixels[y][x][0=R,1=G,2=B]
     private int[][][] pixels;
+    private byte[][][] pixels_bin;
 
     public int getWidth() { return width; }
     public int getHeight() { return height; }
@@ -17,6 +19,7 @@ public class Image {
         this.width = width;
         this.height = height;
         pixels = new int[height][width][3];
+        pixels_bin = new byte[height][width][3]; 
     }
 
     /**
@@ -27,6 +30,10 @@ public class Image {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
+
+            pixels_bin[y][x][0] = (byte) r;
+            pixels_bin[y][x][1] = (byte) g;
+            pixels_bin[y][x][2] = (byte) b;
         }
     }
 
@@ -51,7 +58,22 @@ public class Image {
             }
         }
     }
-    static public read_txt(String filename) throws IOException {
-        //TODO
+
+    public void save_bin(String filename) throws IOException {
+        try (FileOutputStream writer = new FileOutputStream(filename)) {
+            
+            writer.write("P6\n".getBytes());
+            writer.write((width + " " + height + "\n").getBytes());
+            writer.write("255\n".getBytes());
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    
+                    writer.write(pixels_bin[y][x]);
+                }
+               
+            }
+        }
     }
+  
 }
