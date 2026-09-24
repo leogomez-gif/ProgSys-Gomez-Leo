20842a9ecc9c8a86987bf34d964f28905327468c
Leo Gomez <leo.gomez@iut-rodez.fr>
2026-09-24 17:58:00 +0200

    Etape 2 et motié de l'etape 3

--- VFS/Utils.java
+++ VFS/Utils.java
@@ -1,8 +1,9 @@
+
+
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
-        // à partir de 'offset', en big-endian.
+       
         memory[offset] = (byte) (value >> 24);
         memory[offset+1] = (byte) (value >> 16);
         memory[offset+2] = (byte) (value >> 8);
@@ -12,21 +13,81 @@ public class Utils {
 
     public static int readInt(byte[] memory, int offset) {
         int resultat = ((memory[offset] & 0xFF) << 24)
-                         | ((memory[offset + 1] & 0xFF) << 16)
-                         | ((memory[offset + 2] & 0xFF) << 8)
-                         | (memory[offset + 3] & 0xFF);
+                         | ((memory[offset+1] & 0xFF) << 16)
+                         | ((memory[offset+2] & 0xFF) << 8)
+                         | (memory[offset+3] & 0xFF);
 
         return resultat;
         
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
+        memory[offset] = (byte) (value >> 8);
+        memory[offset+1] = (byte) (value >> 0);
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
-        return 0;
+        int resultat = ((memory[offset] & 0xFF) << 8)
+                          | (memory[offset+1] & 0xFF);
+
+        return (short) resultat;
     }
+    public static int writeLong(byte[] memory, int offset, long value) {
+        memory[offset] = (byte) (value >> 56);
+        memory[offset] = (byte) (value >> 48);
+        memory[offset] = (byte) (value >> 40);
+        memory[offset] = (byte) (value >> 32);
+        memory[offset+1] = (byte) (value >> 24);
+        memory[offset+2] = (byte) (value >> 16);
+        memory[offset+3] = (byte) (value >> 8);
+        memory[offset+3] = (byte) (value >> 0);
+
+    return 8;
+        }
+
+    public static long readLong(byte[] memory, int offset) {
+        long resultat = ((memory[offset] & 0xFF) << 56)
+                            | ((memory[offset+1] & 0xFF) << 48)
+                            | ((memory[offset+2] & 0xFF) << 40)
+                            | ((memory[offset+2] & 0xFF) << 32)
+                            | ((memory[offset+2] & 0xFF) << 24)
+                            | ((memory[offset+2] & 0xFF) << 16)
+                            | ((memory[offset+2] & 0xFF) << 8)
+                            | (memory[offset+3] & 0xFF);
+        return resultat;
+    }
+
+    public static int writeString(
+        byte[] memory,
+        int offset,
+        String str,
+        int maxLength) {
+
+
+        byte[] bit = str.getBytes();
+        for (int i=0; i < maxLength; i++)
+
+            if (i < maxLength - 1) {
+                memory[offset + i] = bit[i];
+            } else {
+                memory[offset + i] = 0;        
+            }
+                
+
+
+        return maxLength;
+    }
+
+    public static String readString(
+        byte[] memory,
+        int offset,
+        int maxLength) {
+
+        // TODO:
+        // Lire jusqu'au premier octet nul
+        // ou jusqu'à maxLength.
+
+        return "";
+    } 
 }
